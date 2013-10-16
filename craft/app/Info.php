773c523ad7ac16aadf6cc5c4400052be68f11fc5
Ronan Brett <purplejunkie.com@gmail.com>
2013-10-16 14:28:41 +1100

    init commit

---
+++ craft/app/Info.php
@@ -0,0 +1,18 @@
+<?php
+namespace Craft;
+
+/**
+ * Craft by Pixel & Tonic
+ *
+ * @package   Craft
+ * @author    Pixel & Tonic, Inc.
+ * @copyright Copyright (c) 2013, Pixel & Tonic, Inc.
+ * @license   http://buildwithcraft.com/license Craft License Agreement
+ * @link      http://buildwithcraft.com
+ */
+
+define('CRAFT_VERSION', '1.2');
+define('CRAFT_BUILD', '2367');
+define('CRAFT_RELEASE_DATE', '1381354341');
+define('CRAFT_MIN_BUILD_REQUIRED', '2321');
+define('CRAFT_TRACK', 'stable');
