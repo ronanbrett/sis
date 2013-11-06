e4f53a37af25b4f351d88e840dbc12fccc71f53d
Ronan Brett <purplejunkie.com@gmail.com>
2013-11-06 15:57:05 +1100

    temp

--- craft/config/db.php
+++ craft/config/db.php
@@ -18,10 +18,10 @@ return array(
         'database' => 'craft',
     ),
     '.net' => array(
-        'server' => 'us-cdbr-azure-west-b.cleardb.com',
-        'user' => 'b2a3c54e2decab',
-        'password' => '62caa123',
-        'database' => 'as_c5d075f7c574d83',
+        'server' => 'localhost:3306',
+        'user' => 'root',
+        'password' => 'Ao21Ao21',
+        'database' => 'craft',
     ),
 );
 
